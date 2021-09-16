bc9b0db51950a6a8d2594c6d4cd5ac9d4eadb322
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-09-16 16:19:36 +0300

    fixed error

--- controllers/Weather.Controller.js
+++ controllers/Weather.Controller.js
@@ -5,7 +5,7 @@ const WeatherModel = require('../models/Weather.Model');
 let handleWeatherAPI = async (req, res) => {
   let latitude = req.query.latitude;
   let longitude = req.query.longitude;
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${latitude}&lon=${longitude}&`;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${latitude}&lon=${longitude}`;
   let response = await axios.get(url);
   let respondeData = response.data.map(item => {
     return new WeatherModel(item.datetime, item.weather.description);
