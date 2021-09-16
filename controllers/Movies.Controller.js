7eaab092ff72d2997ebaaf8b048d3eba0f8f68ac
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-09-16 16:09:36 +0300

    fixed server errors

--- controllers/Movies.Controller.js
+++ controllers/Movies.Controller.js
@@ -4,7 +4,7 @@ const MoviesModel = require('../models/Movies.Model');
 
 let handleMoviesAPI = async (req, res) => {
   let place = req.query.place;
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query='${place}''`;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${place}`;
   let response = await axios.get(url);
   let responseData = response.map(item => {
     return new MoviesModel(item.title, item.overview, item.vote_average, item.vote_count, item.backdrop_path, item.popularity, item.release_date);
